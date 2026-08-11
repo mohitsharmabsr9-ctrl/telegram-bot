45a5407dbc2d968793869152dba76ef14dec0639
mohitsharmabsr9-ctrl <mohitsharmabsr9@gmail.com>
2026-08-11 11:32:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def keep_alive():
 keep_alive()
 
 # Telegram Bot Token (Render Environment Variable)
-TELEGRAM_BOT_TOKEN = os.environ.get("8650177978:AAHDCRG6yWFgcv_C8ne_tPP1gLHLYAKsb9U")
+TELEGRAM_BOT_TOKEN = os.environ.get("8650177978:AAFwygvU4vmvU-h_ML3MoGrHJ9LcBzn1jBE")
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 def clean_df(df):
